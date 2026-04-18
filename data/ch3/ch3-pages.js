455d5713f105d190227dcf25e13c1c7ec5eb5295
Qutaiba Salama <qwerfans11@gmail.com>
2026-04-18 11:15:43 +0300

    Add files via upload

---
+++ data/ch3/ch3-pages.js
@@ -0,0 +1,21 @@
+(function(g){
+  g.CH3_PAGE_IMAGE_MAP = {
+    "14": ["Page14_Fig. 3.28 AP abdomen (KUB).png", "Page14_Fig. 3.29 AP (upper) abdomen-landscape.png", "Page14_Fig. 3.30 AP (lower) abdomen-portrait.png"],
+    "15": ["Page15_Fig. 3.31 AP abdomen (KUB).png", "Page15_Fig. 3.32 AP abdomen.png"],
+    "16": ["Page16_Fig. 3.33 PA abdomen.png", "Page16_Fig. 3.34 PA abdomen.png", "Page16_Fig. 3.35 PA abdomen.png"],
+    "17": ["Page17_Fig. 3.36 Left lateral decubitus position (AP).png", "Page17_Fig. 3.37 Left lateral decubitus (AP). (Modified from McQuillen.png", "Page17_Fig. 3.38 Left lateral decubitus (AP). (From McQuillen Martensen K_.png"],
+    "18": ["Page18_Fig. 3.39 Erect AP-to include diaphragm.png", "Page18_Fig. 3.40 Erect AP-to include diaphragm. Bowel obstruction is.png", "Page18_Fig. 3.41 Erect AP.png"],
+    "19": ["Page19_Fig. 3.42 Dorsal decubitus-right lateral position.png", "Page19_Fig. 3.43 Dorsal decubitus-right lateral position.png", "Page19_Fig. 3.44 Dorsal decubitus-right lateral position.png"],
+    "20": ["Page20_Fig. 3.45 Right lateral abdomen.png", "Page20_Fig. 3.46 Right lateral abdomen.png", "Page20_Fig. 3.47 Right lateral abdomen.png"],
+    "21": ["Page21_Fig. 3.48 AP supine.png", "Page21_Fig. 3.49 PA chest erect.png", "Page21_Fig. 3.50 AP erect.png", "Page21_Fig. 3.51 Left lateral decubitus (special projection, if patient cannot.png"]
+  };
+  g.CH3_POSITION_PAGE_START = {
+    "AP Projection - Supine Position (KUB)": 14,
+    "PA Projection - Prone Position (Abdomen)": 16,
+    "Lateral Decubitus Position (AP Projection) - Abdomen": 17,
+    "AP Projection - Erect Position (Abdomen)": 18,
+    "Dorsal Decubitus Position (Right or Left Lateral) - Abdomen": 19,
+    "Lateral Position - Abdomen": 20,
+    "Acute Abdominal Series - Acute Abdomen": 21
+  };
+})(typeof window !== 'undefined' ? window : globalThis);
